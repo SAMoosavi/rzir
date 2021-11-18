4c62e71e0961e8d57eece4f6301a76d7f62c4a97
Ali.Moosavi <moosavi238@gmail.com>
2021-11-18 19:31:36 +0330

    checkbox

--- resources/js/Jetstream/Checkbox.vue
+++ resources/js/Jetstream/Checkbox.vue
@@ -1,6 +1,6 @@
 <template>
     <input type="checkbox" :value="value" v-model="proxyChecked"
-           class="rounded border-gray-300 text-indigo-600 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50">
+           class="text-black border-gray-300 rounded shadow-sm focus:border-none focus:ring-gray-100 focus:ring-opacity-50">
 </template>
 
 <script>

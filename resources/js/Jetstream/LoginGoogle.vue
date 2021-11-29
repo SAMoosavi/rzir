9244fbf5ed66fc5e088cdb70177dbd17900c6512
Ali.Moosavi <moosavi238@gmail.com>
2021-11-29 17:02:02 +0330

    okey color google login

--- resources/js/Jetstream/LoginGoogle.vue
+++ resources/js/Jetstream/LoginGoogle.vue
@@ -1,6 +1,6 @@
 <template>
   <button
-    class="inline-flex items-center px-4 py-2 text-xs tracking-widest text-white uppercase transition bg-red-700 border border-transparent rounded-md  hover:bg-red-300 hover:text-gray-700 active:bg-red-900 focus:outline-none focus:border-red-900 focus:ring focus:ring-red-300 disabled:opacity-25"
+    class="text-white bg-red-500 hover:bg-red-300 hover:text-gray-700 active:bg-red-900 hover:shadow-lg dark:bg-red-700 dark:hover:bg-red-900 dark:hover:text-gray-200"
     type="button"
   >
     <Link :href="route('social.oauth', 'google')"> ورود با حساب گوگل </Link>
@@ -14,3 +14,27 @@ export default {
   },
 };
 </script>
+
+<style scoped>
+
+button {
+  letter-spacing: -0.2px;
+  font-size: 16px;
+}
+
+button {
+  border: 0;
+  outline: 0;
+  font-size: 16px;
+  border-radius: 29px;
+  padding: 16px;
+}
+
+button {
+  font-weight: bold;
+  transition: all 0.2s ease-in-out;
+  cursor: pointer;
+  font-weight: 600;
+}
+
+</style>

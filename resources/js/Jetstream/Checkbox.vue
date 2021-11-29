191154e17f928cfe9e2b7a6e64b5629b5f9865ef
Ali.Moosavi <moosavi238@gmail.com>
2021-11-29 17:09:37 +0330

    okey color check box

--- resources/js/Jetstream/Checkbox.vue
+++ resources/js/Jetstream/Checkbox.vue
@@ -1,6 +1,6 @@
 <template>
     <input type="checkbox" :value="value" v-model="proxyChecked"
-           class="text-black border-gray-300 rounded shadow-sm focus:border-none focus:ring-gray-100 focus:ring-opacity-50">
+           class="text-yellow-300 border-gray-300 rounded shadow-sm focus:border-none focus:ring-gray-100 focus:ring-opacity-50 dark:text-yellow-400 dark:focus:ring-gray-600 dark:border-gray-600">
 </template>
 
 <script>

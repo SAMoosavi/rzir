5204b8367a977dcdf9ac76dd29d89e8f6ab3e6b0
Ali.Moosavi <moosavi238@gmail.com>
2021-11-29 15:32:14 +0330

    okey input component

--- resources/js/Jetstream/Input.vue
+++ resources/js/Jetstream/Input.vue
@@ -1,19 +1,116 @@
 <template>
-    <input class="border-gray-300 rounded-md shadow-sm focus:ring focus:ring-black focus:ring-opacity-50" :value="modelValue" @input="$emit('update:modelValue', $event.target.value)" ref="input">
+  <label>
+    <input
+      :value="modelValue"
+      @input="$emit('update:modelValue', $event.target.value)"
+      ref="input"
+      :id="id"
+      :type="type"
+      class="bg-indigo-200 dark:bg-indigo-800"
+      :class="classItem"
+    />
+  </label>
 </template>
 
 <script>
-    import { defineComponent } from 'vue'
+import { defineComponent } from "vue";
 
-    export default defineComponent({
-        props: ['modelValue'],
+export default defineComponent({
+  props: ["modelValue","id","type","classItem"],
 
-        emits: ['update:modelValue'],
+  emits: ["update:modelValue"],
 
-        methods: {
-            focus() {
-                this.$refs.input.focus()
-            }
-        }
-    })
+  methods: {
+    focus() {
+      this.$refs.input.focus();
+    },
+  },
+});
 </script>
+
+<style scoped>
+body,
+p,
+input,
+select,
+textarea,
+button {
+  font-family: "Montserrat", sans-serif;
+  letter-spacing: -0.2px;
+  font-size: 16px;
+}
+button,
+input {
+  border: 0;
+  outline: 0;
+  font-size: 16px;
+  border-radius: 320px;
+  padding: 16px;
+}
+
+label {
+  display: block;
+  margin-bottom: 24px;
+  width: 100%;
+}
+
+input {
+  margin-right: 8px;
+  box-shadow: inset 0px 2px 5px #babecc, inset 0px -5px 10px #fff;
+  width: 100%;
+  box-sizing: border-box;
+  transition: all 0.2s ease-in-out;
+  appearance: none;
+  -webkit-appearance: none;
+}
+input:focus {
+  box-shadow: inset 1px 1px 2px #babecc, inset -1px -1px 2px #fff;
+}
+
+button {
+  color: #61677c;
+  font-weight: bold;
+  box-shadow: -5px -5px 20px #fff, 5px 5px 20px #babecc;
+  transition: all 0.2s ease-in-out;
+  cursor: pointer;
+  font-weight: 600;
+}
+button:hover {
+  box-shadow: -2px -2px 5px #fff, 2px 2px 5px #babecc;
+}
+button:active {
+  box-shadow: inset 1px 1px 2px #babecc, inset -1px -1px 2px #fff;
+}
+button .icon {
+  margin-right: 8px;
+}
+button.unit {
+  border-radius: 8px;
+  line-height: 0;
+  width: 48px;
+  height: 48px;
+  display: inline-flex;
+  justify-content: center;
+  align-items: center;
+  margin: 0 8px;
+  font-size: 19.2px;
+}
+button.unit .icon {
+  margin-right: 0;
+}
+button.red {
+  display: block;
+  width: 100%;
+  color: #ae1100;
+}
+
+.input-group {
+  display: flex;
+  align-items: center;
+  justify-content: flex-start;
+}
+.input-group label {
+  margin: 0;
+  flex: 1;
+}
+</style>

864d6b89584ebf03746b1fc6bca64d6d64048a7f
Ali.Moosavi <moosavi238@gmail.com>
2021-11-29 17:02:28 +0330

    okey color button

--- resources/js/Jetstream/Button.vue
+++ resources/js/Jetstream/Button.vue
@@ -1,18 +1,47 @@
 <template>
-    <button :type="type" class="inline-flex items-center px-4 py-2 text-xs font-semibold tracking-widest text-white uppercase transition bg-black border border-transparent rounded-md hover:bg-gray-600 active:bg-gray-900 focus:outline-none focus:border-gray-900 focus:ring focus:ring-gray-300 disabled:opacity-25">
-        <slot></slot>
+
+    <button
+      :type="type"
+      class="bg-yellow-300 hover:bg-yellow-100 dark:bg-yellow-400 dark:hover:bg-yellow-600 dark:focus:bg-yellow-600 hover:shadow-lg "
+      :class="classItem"
+    >
+      <slot></slot>
     </button>
 </template>
 
 <script>
-    import { defineComponent } from 'vue'
+import { defineComponent } from "vue";
 
-    export default defineComponent({
-        props: {
-            type: {
-                type: String,
-                default: 'submit',
-            },
-        }
-    })
+export default defineComponent({
+  props: {
+    type: {
+      type: String,
+      default: "submit",
+    },
+  },
+});
 </script>
+<style scoped>
+
+button {
+  letter-spacing: -0.2px;
+  font-size: 16px;
+}
+
+button {
+  border: 0;
+  outline: 0;
+  font-size: 16px;
+  border-radius: 29px;
+  padding: 16px;
+}
+
+button {
+  font-weight: bold;
+  transition: all 0.2s ease-in-out;
+  cursor: pointer;
+  font-weight: 600;
+}
+
+</style>
+

21ea618c6f2c871e24c2e0d2fdb9f33a074665b5
Ali.Moosavi <moosavi238@gmail.com>
2021-11-30 08:40:21 +0330

    okey color danger button

--- resources/js/Jetstream/DangerButton.vue
+++ resources/js/Jetstream/DangerButton.vue
@@ -1,5 +1,5 @@
 <template>
-    <button :type="type" class="inline-flex items-center justify-center px-4 py-2 bg-red-600 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-red-500 focus:outline-none focus:border-red-700 focus:ring focus:ring-red-200 active:bg-red-600 disabled:opacity-25 transition">
+    <button :type="type" class="inline-flex items-center text-xs tracking-widest text-gray-700 uppercase transition bg-red-500 rounded-md shadow-sm hover:shadow-lg active:text-gray-800 active:bg-yellow-50 disabled:opacity-25 hover:bg-red-400 dark:bg-red-600 dark:hover:bg-red-800">
         <slot></slot>
     </button>
 </template>
@@ -16,3 +16,26 @@
         }
     })
 </script>
+<style scoped>
+
+button {
+  letter-spacing: -0.2px;
+  font-size: 16px;
+}
+
+button {
+  border: 0;
+  outline: 0;
+  font-size: 16px;
+  border-radius: 29px;
+  padding: 16px;
+}
+
+button {
+  font-weight: bold;
+  transition: all 0.2s ease-in-out;
+  cursor: pointer;
+  font-weight: 600;
+}
+
+</style>

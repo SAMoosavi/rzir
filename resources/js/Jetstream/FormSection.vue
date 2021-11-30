dbb57c1c373b7f47423d96c80e620486e383d19d
Ali.Moosavi <moosavi238@gmail.com>
2021-11-30 09:14:57 +0330

    okey form section

--- resources/js/Jetstream/FormSection.vue
+++ resources/js/Jetstream/FormSection.vue
@@ -7,7 +7,7 @@
 
         <div class="mt-5 md:mt-0 md:col-span-2">
             <form @submit.prevent="$emit('submitted')">
-                <div class="px-4 py-5 bg-red-600 bg-indigo-200 shadow dark:bg-indigo-800 sm:p-6"
+                <div class="px-4 py-5 bg-indigo-200 shadow dark:bg-indigo-800 sm:p-6"
                     :class="hasActions ? 'sm:rounded-tl-md sm:rounded-tr-md' : 'sm:rounded-md'">
                     <div class="grid grid-cols-6 gap-6 ">
                         <slot name="form"></slot>

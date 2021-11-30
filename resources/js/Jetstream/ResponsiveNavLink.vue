73f3a0b8d2a51838586b6b5f4ebc4b084da19464
Ali.Moosavi <moosavi238@gmail.com>
2021-11-30 09:28:12 +0330

    okey color responsive nave link

--- resources/js/Jetstream/ResponsiveNavLink.vue
+++ resources/js/Jetstream/ResponsiveNavLink.vue
@@ -23,8 +23,8 @@
         computed: {
             classes() {
                 return this.active
-                    ? 'block pl-3 pr-4 py-2 border-l-4 border-indigo-400 text-base font-medium text-indigo-700 bg-indigo-50 focus:outline-none focus:text-indigo-800 focus:bg-indigo-100 focus:border-indigo-700 transition dark:text-white  dark:bg-indigo-800 dark:border-indigo-500'
-                    : 'block pl-3 pr-4 py-2 border-l-4 border-transparent text-base font-medium text-gray-600 hover:text-indigo-800 hover:bg-indigo-50 hover:border-indigo-300 focus:outline-none focus:text-indigo-800 focus:bg-indigo-50 focus:border-indigo-300 transition dark:text-gray-400 '
+                    ? 'block pl-3 pr-4 py-2 border-l-4 border-indigo-400 text-base font-medium text-indigo-700 bg-indigo-50 focus:outline-none focus:text-indigo-800 focus:bg-indigo-100 focus:border-indigo-700 transition dark:text-white  dark:bg-indigo-600 dark:border-indigo-400'
+                    : 'block pl-3 pr-4 py-2 border-l-4 border-transparent text-base font-medium text-gray-600 hover:text-indigo-800 hover:bg-indigo-50 hover:border-indigo-300 focus:outline-none focus:text-indigo-800 focus:bg-indigo-50 focus:border-indigo-300 transition dark:text-gray-400 dark:hover:bg-indigo-700 dark:hover:border-indigo-300 '
             }
         }
     })

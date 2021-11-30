9285c92fe5335929a28d460c8279207439e77579
Ali.Moosavi <moosavi238@gmail.com>
2021-11-30 09:20:43 +0330

    okey color dropdown link

--- resources/js/Jetstream/DropdownLink.vue
+++ resources/js/Jetstream/DropdownLink.vue
@@ -1,14 +1,14 @@
 <template>
     <div>
-        <button type="submit" class="block w-full px-4 py-2 text-sm leading-5 text-left text-gray-700 transition hover:bg-gray-100 focus:outline-none focus:bg-gray-100 dark:text-gray-100 dark:hover:bg-gray-600" v-if="as == 'button'">
+        <button type="submit" class="block w-full px-4 py-2 text-sm leading-5 text-left text-gray-700 transition hover:bg-indigo-50 focus:outline-none focus:bg-indigo-50 dark:text-gray-100 dark:hover:bg-indigo-600" v-if="as == 'button'">
             <slot></slot>
         </button>
 
-        <a :href="href" class="block px-4 py-2 text-sm leading-5 text-gray-700 transition hover:bg-gray-100 focus:outline-none focus:bg-gray-100 dark:text-gray-100 dark:hover:bg-gray-600" v-else-if="as =='a'">
+        <a :href="href" class="block px-4 py-2 text-sm leading-5 text-gray-700 transition hover:bg-indigo-50 focus:outline-none focus:bg-indigo-50 dark:text-gray-100 dark:hover:bg-indigo-600" v-else-if="as =='a'">
             <slot></slot>
         </a>
 
-        <Link :href="href" class="block px-4 py-2 text-sm leading-5 text-gray-700 transition hover:bg-gray-100 focus:outline-none focus:bg-gray-100 dark:text-gray-100 dark:hover:bg-gray-600" v-else>
+        <Link :href="href" class="block px-4 py-2 text-sm leading-5 text-gray-700 transition hover:bg-indigo-50 focus:outline-none focus:bg-indigo-50 dark:text-gray-100 dark:hover:bg-indigo-600" v-else>
             <slot></slot>
         </Link>
     </div>
